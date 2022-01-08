be867a13a80dafacd9c2034ef6e2cc8f6dbe2a5c
kenji424 <46304856+kenji424@users.noreply.github.com>
2022-01-08 19:30:44 +0900

    Create create_a_list_of_all_files.py

---
+++ create_a_list_of_all_files.py
@@ -0,0 +1,15 @@
+# create a list of all files
+
+import glob
+
+files = glob.glob('C:/Users/kawam/Documents/Nomura/archive/*')
+
+f = open('C:/Users/kawam/Documents/Nomura/usd-list.csv', 'a')
+
+
+for file in files:
+    
+    if 'usd.' in file:
+        f.write(file[40:] + '\n')
+
+f.close()
