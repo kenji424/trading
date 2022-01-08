36d33abb1c60af01ede7901787a13e615ef998f3
kenji424 <46304856+kenji424@users.noreply.github.com>
2022-01-08 19:46:21 +0900

    Add files via upload

---
+++ create_a_summary_from_a_list.py
@@ -0,0 +1,101 @@
+# create a summary from a list
+
+# open a currency list file
+f = open('C:/Users/kawam/Documents/Nomura/analysis/usd-list.csv', 'r')
+file_list = f.readlines()
+f.close()
+
+cur_name = []
+start_time = []
+end_time = []
+
+days = []
+
+open_price = []
+close_price = []
+high_price = []
+low_price = []
+
+trade_values = []
+daily_trade_values = []
+
+
+for i in range(len(file_list)):
+#for i in range(10):
+
+    cur_name.append(file_list[i][:len(file_list[i]) - 5])
+    
+    f = open('C:/Users/kawam/Documents/Nomura/archive/' + file_list[i].rstrip('\r\n'), 'r')
+    
+    data_list = f.readlines()
+    
+    trade_value = 0.0
+    high = 0.0
+    low = 10**20
+    
+    for j in range(1, len(data_list)):
+        
+        inputs = list(data_list[j].split(','))
+        
+        tm = int(inputs[0])
+        op = float(inputs[1])
+        cl = float(inputs[2])
+        hi = float(inputs[3])
+        lo = float(inputs[4])
+        vol = float(inputs[5])
+        
+        if j == 1:
+            st = tm
+            start_time.append(tm)
+            open_price.append(op)
+            
+        if hi > high:
+            high = hi
+        
+        if lo < low:
+            low = lo
+        
+        trade_value += cl * vol
+        
+
+    end_time.append(tm)
+    
+    day = int((tm - st) / 86400000)
+    
+    days.append(day)
+    
+    close_price.append(cl)
+    high_price.append(high)
+    low_price.append(low)
+
+    trade_values.append(trade_value)
+    
+    daily_trade_values.append(trade_value / day)
+    
+    
+
+
+# put data into a summary file
+    
+f = open('C:/Users/kawam/Documents/Nomura/analysis/usd-Summary.csv', 'w')
+
+f.write('Currency,StartTime,EndTime,Days,Open,Close,High,Low,TradeValue,DailyTradeValue\n')
+
+for i in range(len(file_list)):
+#for i in range(10):
+     
+    f.write(cur_name[i] + ',' + str(start_time[i]) + ',' + str(end_time[i]) + ',' + 
+            str(days[i]) + ',' + str(open_price[i]) + ',' + str(close_price[i]) + ',' + 
+            str(high_price[i]) + ',' + str(low_price[i]) + ',' + str(trade_values[i]) + ',' + 
+            str(daily_trade_values[i]) + '\n')
+
+f.close()
+
+            
+
+
+
+
+
+
+
